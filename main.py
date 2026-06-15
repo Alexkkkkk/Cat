945ad9a6069f7db6851b7a3de84b7ced0bf2195d
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-06-15 20:48:13 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import uvicorn
+
+app = FastAPI()
+
+# Раздача статики (index.html, картинки)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
