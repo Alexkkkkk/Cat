a93d48a86214e9f24a68e69a158b40f1256d0065
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-06-15 21:50:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def read_index():
     return FileResponse('static/index.html')
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
