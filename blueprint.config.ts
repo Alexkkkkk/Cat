a0302fdb15e7b92e070b7a5b905f9897565ba229
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-06-17 21:33:13 +0300

    Create blueprint.config.ts

---
+++ blueprint.config.ts
@@ -0,0 +1,8 @@
+import { Config } from '@ton/blueprint';
+
+export const config: Config = {
+    network: {
+        endpoint: 'https://toncenter.com/api/v2/jsonRPC', // Эндпоинт для Mainnet
+        type: 'mainnet',
+    },
+};
