31bd8e23aad88879ef057f548c77c8446c3bb204
Alexkkkkk <102665324+Alexkkkkk@users.noreply.github.com>
2026-06-18 07:27:01 +0300

    Delete blueprint.config.ts

--- blueprint.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from '@ton/blueprint';
-
-export const config: Config = {
-    network: {
-        endpoint: 'https://toncenter.com/api/v2/jsonRPC', // Эндпоинт для Mainnet
-        type: 'mainnet',
-    },
-};
